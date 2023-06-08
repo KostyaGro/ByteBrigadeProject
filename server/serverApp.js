1e264b2942ca01b4d2a5d7dab3392162c571f0b4
Александр Шмелев <120136549+Felarn@users.noreply.github.com>
2023-06-08 13:10:27 +0300

    server response with index.html

--- server/serverApp.js
+++ server/serverApp.js
@@ -1,9 +1,17 @@
+import fs from "fs";
+import path from "path";
 import router from "./router.js";
 
-export default (settings) => (request, response) => {
-  console.log(request);
-  console.log(settings);
+export default (config) => (request, response) => {
+  console.log(request.url);
+  console.log(config);
   console.log(router);
+
   response.writeHead(200);
-  response.end("hi");
+  const data = fs.readFileSync(
+    path.resolve(config.dirname, config.sitePath, "index.html")
+  );
+
+  // response.end("hi");
+  response.end(data, "binary");
 };
