b82383e24a80e2dac30861393e898b5bdaa773f0
Александр Шмелев <120136549+Felarn@users.noreply.github.com>
2023-06-08 13:22:40 +0300

    testing procedural response

--- server/serverApp.js
+++ server/serverApp.js
@@ -7,10 +7,25 @@ export default (config) => (request, response) => {
   console.log(config);
   console.log(router);
 
+  let data = "";
   response.writeHead(200);
-  const data = fs.readFileSync(
-    path.resolve(config.dirname, config.sitePath, "index.html")
-  );
+  if (request.url === "/") {
+    data = fs.readFileSync(
+      path.resolve(config.dirname, config.sitePath, "index.html")
+    );
+  } else {
+    try {
+      const fullPath = path.resolve(
+        config.dirname,
+        config.sitePath,
+        request.url.slice(1)
+      );
+      console.log(fullPath);
+      data = fs.readFileSync(fullPath);
+    } catch (e) {
+      console.log(`can't read address: "${request.url}"`);
+    }
+  }
 
   // response.end("hi");
   response.end(data, "binary");
