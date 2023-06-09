085228b02accf4ef0fa628a6f70d815c7a40ae62
Александр Шмелев <120136549+Felarn@users.noreply.github.com>
2023-06-10 00:07:29 +0300

    style fixes

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
+import fs from 'fs';
+import path from 'path';
+import http from 'http';
+import serverApp from './server/serverApp.js';
 
-import fs from "fs";
-import path from "path";
-import http from "http";
-import serverApp from "./server/serverApp.js";
 console.clear();
 
 const __dirname = process.cwd();
 const config = JSON.parse(
-  fs.readFileSync(path.resolve(__dirname, "config.json"))
+  fs.readFileSync(path.resolve(__dirname, 'config.json')),
 );
 config.dirname = __dirname;
 const app = serverApp(config);
@@ -17,4 +17,3 @@ console.log(config);
 http.createServer(app).listen(config.port, () => {
   console.log(`server started. Listening on ${config.port}`);
 });
-
--- server/serverApp.js
+++ server/serverApp.js
@@ -1,51 +1,51 @@
-import fs from "fs";
-import path from "path";
-import router from "./router.js";
-import { URL } from "url";
+import fs from 'fs';
+import path from 'path';
+import { URL } from 'url';
+import router from './router.js';
 
-const formatHtml = (htmlPage) =>
-  String(htmlPage)
-    .replace(/(?<=href="\S+)\.html(?=")/g, "")
-    .replace(/(?<=href="\S+)index(?=")/g, "");
+const formatHtml = (htmlPage) => String(htmlPage)
+  .replace(/(?<=href="\S+)\.html(?=")/g, '')
+  .replace(/(?<=href="\S+)index(?=")/g, '');
 
-const isLegitTarget = (targetPath) =>
-  fs.existsSync(targetPath) && fs.statSync(targetPath).isFile();
+const isLegitTarget = (targetPath) => fs.existsSync(targetPath) && fs.statSync(targetPath).isFile();
 
 export default (config) => (request, response) => {
   console.log(`> request: ${request.method}  ${request.url}`);
   const url = new URL(request.url, `http://${request.headers.host}`);
-  const searchParams = url.searchParams;
+  const { searchParams } = url;
   let relativePath = url.pathname;
   console.log(
-    `looking for relative path: "${relativePath}"  with searchparams: "${searchParams}"`
+    `looking for relative path: "${relativePath}"  with searchparams: "${searchParams}"`,
   );
 
   //  ============= вывести в функцию router.js ===========
   // ------------- поиск и отдача элементов сайта из папки "./site"
-  if (relativePath.at(-1) === "/") relativePath += "index";
+  if (relativePath.at(-1) === '/') relativePath += 'index';
 
-  let data = "";
+  let data = '';
   const absolutePath = path.resolve(
     config.dirname,
     config.sitePath,
-    `.${relativePath}`
+    `.${relativePath}`,
   );
   if (isLegitTarget(absolutePath)) {
     response.writeHead(200);
     data = fs.readFileSync(absolutePath);
-    response.end(data, "binary");
+    response.end(data, 'binary');
     return true;
   }
 
-  const absolutePathHtml = absolutePath + ".html";
+  const absolutePathHtml = `${absolutePath}.html`;
   if (isLegitTarget(absolutePathHtml)) {
-    response.writeHead(200, { ContentType: "text/html" });
+    response.writeHead(200, { ContentType: 'text/html' });
     data = fs.readFileSync(absolutePathHtml);
     response.end(formatHtml(data));
     return true;
   }
+
   // ================================================
   console.log(
-    `site resource isn't found on: ${request.url} | ${absolutePath} | ${absolutePathHtml}`
+    `site resource isn't found on: ${request.url} | ${absolutePath} | ${absolutePathHtml}`,
   );
+  return false;
 };
