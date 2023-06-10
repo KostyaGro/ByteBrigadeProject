c503f7654723d8ade5a459145f0246fccdfc698e
Александр Шмелев <120136549+Felarn@users.noreply.github.com>
2023-06-11 02:03:36 +0300

    router separated

--- server/serverApp.js
+++ server/serverApp.js
@@ -1,51 +1,30 @@
-import fs from 'fs';
-import path from 'path';
 import { URL } from 'url';
-import router from './router.js';
-
-const formatHtml = (htmlPage) => String(htmlPage)
-  .replace(/(?<=href="\S+)\.html(?=")/g, '')
-  .replace(/(?<=href="\S+)index(?=")/g, '');
-
-const isLegitTarget = (targetPath) => fs.existsSync(targetPath) && fs.statSync(targetPath).isFile();
+import staticRouter from './routers/staticRouter.js';
+import apiRouter from './routers/apiRoter.js';
+// import router from './routers/staticRouter.js';
 
 export default (config) => (request, response) => {
-  console.log(`> request: ${request.method}  ${request.url}`);
+  console.log(`> request: ${request.method}  ${request.url}  ${request.query}`);
+  console.log(`cookie: ${request.headers.cookie}`);
   const url = new URL(request.url, `http://${request.headers.host}`);
   const { searchParams } = url;
-  let relativePath = url.pathname;
+  const relativePath = url.pathname;
   console.log(
     `looking for relative path: "${relativePath}"  with searchparams: "${searchParams}"`,
   );
 
-  //  ============= вывести в функцию router.js ===========
-  // ------------- поиск и отдача элементов сайта из папки "./site"
-  if (relativePath.at(-1) === '/') relativePath += 'index';
-
-  let data = '';
-  const absolutePath = path.resolve(
-    config.dirname,
-    config.sitePath,
-    `.${relativePath}`,
-  );
-  if (isLegitTarget(absolutePath)) {
-    response.writeHead(200);
-    data = fs.readFileSync(absolutePath);
-    response.end(data, 'binary');
-    return true;
+  if (request.url.startsWith('/api/')) {
+    const isApiOk = apiRouter(request, response, config);
+    if (!isApiOk) {
+      response.writeHead(400);
+      response.end('400 Bad Reques');
+    }
+    return;
   }
 
-  const absolutePathHtml = `${absolutePath}.html`;
-  if (isLegitTarget(absolutePathHtml)) {
-    response.writeHead(200, { ContentType: 'text/html' });
-    data = fs.readFileSync(absolutePathHtml);
-    response.end(formatHtml(data));
-    return true;
+  const isStaticOk = staticRouter(request, response, relativePath, config);
+  if (!isStaticOk) {
+    response.writeHead(404);
+    response.end('<p style="font-size:500%"> 404: Page not found!</p>');
   }
-
-  // ================================================
-  console.log(
-    `site resource isn't found on: ${request.url} | ${absolutePath} | ${absolutePathHtml}`,
-  );
-  return false;
 };
