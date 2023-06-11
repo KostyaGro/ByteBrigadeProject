fbb9e69deb831883d896f1ed4a2b297204596608
Александр Шмелев <120136549+Felarn@users.noreply.github.com>
2023-06-12 00:47:35 +0300

    added images and brand to card

--- server/products.js
+++ server/products.js
@@ -1,10 +1,20 @@
 // import fs from 'fs';
-// import path from 'path';
+import path from 'path';
+import _ from 'lodash';
 import readObjFromFile from './readObjFromFile.js';
 
 class Products {
   constructor(config) {
     this.all = readObjFromFile(config.productsDBpath);
+    // this.all = {
+    console.log('=============>');
+    this.all = _.merge(
+      this.all,
+      _.mapValues(
+        this.all,
+        (({ image }) => ({ productImgPath: path.resolve(config.productImgDir, image) })),
+      ),
+    );
   }
 
   getByID = (id) => this.all[id];
