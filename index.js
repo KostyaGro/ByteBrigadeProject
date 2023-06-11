b48b890076387e7cee49b0013449ed22a00c8a43
Александр Шмелев <120136549+Felarn@users.noreply.github.com>
2023-06-11 12:38:59 +0300

    [add] config class, and cleaned up index.js

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
-import fs from 'fs';
-import path from 'path';
 import http from 'http';
 import serverApp from './server/serverApp.js';
+import Config from './server/config.js';
 
 // eslint-disable-next-line no-console
 console.clear();
-
-const __dirname = process.cwd();
-const config = JSON.parse(
-  fs.readFileSync(path.resolve(__dirname, 'config.json')),
-);
-config.dirname = __dirname;
+const config = new Config('config.json');
 const app = serverApp(config);
 
 console.log(config);
