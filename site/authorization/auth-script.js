ee25f5bf15165f81c902e271be6fbbe507db69bc
Александр Шмелев <120136549+Felarn@users.noreply.github.com>
2023-06-13 01:50:29 +0300

    moved do separate folder

---
+++ site/authorization/auth-script.js
@@ -0,0 +1,35 @@
+const loginButton = document.querySelector('#login-button');
+
+loginButton.addEventListener('click', async (event) => {
+  event.preventDefault();
+  const loginNameInput = document.querySelector('#loginName-log-in');
+  //   const loginName = loginNameInput.value;
+  const passwordInput = document.querySelector('#password-log-in');
+  const credentials = { loginName: loginNameInput.value, password: passwordInput.value };
+
+  console.log(credentials);
+  fetch('/api/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    body: JSON.stringify(credentials),
+  })
+    .then((response) => {
+      const container = loginButton.closest('div');
+      const messageBox = document.createElement('p');
+      container.append(messageBox);
+      switch (response.status) {
+        case 200:
+          messageBox.textContent = 'успех';
+          break;
+        case 400:
+          messageBox.textContent = 'неверный логин или пароль';
+          break;
+        default:
+          break;
+      }
+    });
+});
+
+const regButton = document.querySelector('#reg-button');
