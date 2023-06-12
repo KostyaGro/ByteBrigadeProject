be92607dba94520566524936c0575e888dfb31ad
Александр Шмелев <120136549+Felarn@users.noreply.github.com>
2023-06-12 23:55:17 +0300

    added addition to usersDB

--- server/users.js
+++ server/users.js
@@ -1,10 +1,33 @@
 // import fs from 'fs';
 // import path from 'path';
+import fs from 'fs';
 import readObjFromFile from './readObjFromFile.js';
 
 class Users {
   constructor(config) {
     this.all = readObjFromFile(config.usersDBpath);
+    this.config = config;
+  }
+
+  static get emptyUser() {
+    return {
+      loginName: '',
+      password: '',
+      email: '',
+      firstName: '',
+      secondName: '',
+      phoneNumber: '',
+      shippingAddress: '',
+      cart: [],
+    };
+  }
+
+  saveDB() { fs.writeFileSync(this.config.usersDBpath, JSON.stringify(this.all, null, 2)); }
+
+  add(credentials) {
+    const lastID = Number(Object.keys(this.all).at(-1) ?? 0);
+    this.all[lastID + 1] = { ...Users.emptyUser, ...credentials };
+    this.saveDB();
   }
 }
 export default Users;
