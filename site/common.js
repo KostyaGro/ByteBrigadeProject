01a3c22b03658549067375d8ca1726079c0cdcd1
Александр Шмелев <120136549+Felarn@users.noreply.github.com>
2023-06-13 12:02:03 +0300

    added files

---
+++ site/common.js
@@ -0,0 +1,3 @@
+console.log('i`m common JS script');
+
+export default () => { console.log('common.js is called'); };
--- site/index.js
+++
@@ -1,3 +0,0 @@
-fetch('./api/').then(async (response) => {
-  console.log(Array.from(response.headers.entries()));
-});
