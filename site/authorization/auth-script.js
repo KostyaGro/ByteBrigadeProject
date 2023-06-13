b1211a58c16cd1b0ae79ac47a350a216cd05a014
Александр Шмелев <120136549+Felarn@users.noreply.github.com>
2023-06-13 14:07:02 +0300

    registration button

--- site/authorization/auth-script.js
+++ site/authorization/auth-script.js
@@ -7,7 +7,7 @@ loginButton.addEventListener('click', async (event) => {
   const passwordInput = document.querySelector('#password-log-in');
   const credentials = { loginName: loginNameInput.value, password: passwordInput.value };
 
-  console.log(credentials);
+  // console.log(credentials);
   fetch('/api/login', {
     method: 'POST',
     headers: {
@@ -37,3 +37,19 @@ loginButton.addEventListener('click', async (event) => {
 });
 
 const regButton = document.querySelector('#reg-button');
+
+regButton.addEventListener('click', async (event) => {
+  event.preventDefault();
+  const regNameInput = document.querySelector('#loginName-reg');
+  //   const loginName = loginNameInput.value;
+  const regPasswordInput = document.querySelector('#password-reg');
+  const regPasswordRepeatInput = document.querySelector('#password-repeat-reg');
+  const credentials = { loginName: regNameInput.value, password: regPasswordInput.value };
+  fetch('/api/user', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    body: JSON.stringify(credentials),
+  });
+});
