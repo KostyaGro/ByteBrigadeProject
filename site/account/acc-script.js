a2d66b012cb76ff9606a756ced7b5ee8b187ba98
Felarn <120136549+Felarn@users.noreply.github.com>
2023-06-28 19:16:01 +0300

    Update acc-script.js
    добавлен вывод фамилии в поле фамилии

--- site/account/acc-script.js
+++ site/account/acc-script.js
@@ -7,6 +7,7 @@ fetchObject('/api/user/')
   .then((resp) => {
     document.querySelector('.login-name p').textContent = resp.loginName;
     document.querySelector('.first-name p').textContent = resp.firstName;
+    document.querySelector('.second-name p').textContent = resp.secondName;
     document.querySelector('.e-mail p').textContent = resp.email;
     // userInfo.textContent = resp;
   });
